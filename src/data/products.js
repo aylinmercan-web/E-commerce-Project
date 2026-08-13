518a1f4f3a70f9b51180256875252974943de654
Aylin Mercan <aylinmercan7@gmail.com>
2026-08-13 22:06:33 +0300

    T04: Product Detail Page

---
+++ src/data/products.js
@@ -0,0 +1,14 @@
+const products = [
+  { id: 1, image: '/product1.png', title: 'Graphic Design', subtitle: 'English Department', oldPrice: 16.48, newPrice: 6.48 },
+  { id: 2, image: '/product2.png', title: 'Graphic Design', subtitle: 'English Department', oldPrice: 16.48, newPrice: 6.48 },
+  { id: 3, image: '/product3.png', title: 'Graphic Design', subtitle: 'English Department', oldPrice: 16.48, newPrice: 6.48 },
+  { id: 4, image: '/product4.png', title: 'Graphic Design', subtitle: 'English Department', oldPrice: 16.48, newPrice: 6.48 },
+  { id: 5, image: '/product5.png', title: 'Graphic Design', subtitle: 'English Department', oldPrice: 16.48, newPrice: 6.48 },
+  { id: 6, image: '/product6.png', title: 'Graphic Design', subtitle: 'English Department', oldPrice: 16.48, newPrice: 6.48 },
+  { id: 7, image: '/product7.png', title: 'Graphic Design', subtitle: 'English Department', oldPrice: 16.48, newPrice: 6.48 },
+  { id: 8, image: '/product8.png', title: 'Graphic Design', subtitle: 'English Department', oldPrice: 16.48, newPrice: 6.48 },
+  { id: 9, image: '/product9.png', title: 'Graphic Design', subtitle: 'English Department', oldPrice: 16.48, newPrice: 6.48 },
+  { id: 10, image: '/product10.png', title: 'Graphic Design', subtitle: 'English Department', oldPrice: 16.48, newPrice: 6.48 },
+]
+
+export default products
